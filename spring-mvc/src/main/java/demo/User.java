810c2053e83aa32dbb327f4cc1742dd18b5fcb3f
Pin-Wang <1228935432@qq.com>
2017-07-24 23:05:15 +0800

    fdf

---
+++ spring-mvc/src/main/java/demo/User.java
@@ -0,0 +1,54 @@
+package demo;
+
+import java.util.Date;
+
+public class User {
+	private Integer id;
+	private String name;
+	private Date birthday;
+	private String sex;
+	private String address;
+	
+	
+	
+	public User(Integer id, String name, Date birthday, String sex, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthday = birthday;
+		this.sex = sex;
+		this.address = address;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+
+}
