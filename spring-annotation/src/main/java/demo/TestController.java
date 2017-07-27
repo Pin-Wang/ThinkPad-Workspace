2140b1f46e40a70625e9843d1047415b1e856c44
Pin-Wang <1228935432@qq.com>
2017-07-27 22:33:14 +0800

    fdf

--- spring-annotation/src/main/java/demo/TestController.java
+++ spring-annotation/src/main/java/demo/TestController.java
@@ -3,6 +3,7 @@ package demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+//test
 @Controller
 @RequestMapping(value = "/test")
 public class TestController
